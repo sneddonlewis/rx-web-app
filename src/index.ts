ce71862970325f0c59eea6c8699801505c730844
Lewis Sneddon <lewis@lewiscodes.io>
2024-01-19 18:59:19 +0000

    playing with rx

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('hi')
+import { tap, Observable, Observer, map } from 'rxjs'
+
+let numbers = [1,5,10]
+let source = new Observable((obs: Observer<number>) => {
+
+  let index = 0
+  let produceValue = () => {
+    obs.next(numbers[index++])
+    if (index < numbers.length) {
+      setTimeout(produceValue, 1000)
+    } else {
+      obs.complete()
+    }
+  }
+  produceValue()
+})
+
+source
+  .pipe(
+    map(x => x * 2),
+    tap(console.log)
+  )
+  .subscribe()
