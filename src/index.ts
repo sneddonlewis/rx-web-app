3192a1153e2c400dac722ff7d4cd837bcdc2d75b
Lewis Sneddon <lewis@lewiscodes.io>
2024-01-19 19:29:49 +0000

    tracked mouse events

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { tap, Observable, Observer, map, take } from 'rxjs'
+import { tap, Observable, Observer, map, take, fromEvent } from 'rxjs'
 
-let source = new Observable((obs: Observer<number>) => {
+const source = new Observable((obs: Observer<number>) => {
 
   let number = 0
   setInterval(() => {
@@ -8,11 +8,28 @@ let source = new Observable((obs: Observer<number>) => {
   }, 1000)
 })
 
+const mouseMoveSource = fromEvent(document, 'mousemove')
+
 const root = document.getElementById('root')
+
+const counter = document.createElement('h3')
+const mouseTrackerX = document.createElement('h3')
+const mouseTrackerY = document.createElement('h3')
+root!.appendChild(counter)
+root!.appendChild(mouseTrackerX)
+root!.appendChild(mouseTrackerY)
 
 source
   .pipe(
-    tap(x => root!.innerText = x.toString()),
+    tap(x => counter.innerText = x.toString()),
     take(5)
   )
   .subscribe()
+
+mouseMoveSource
+  .pipe(
+    tap((mouseEvent: MouseEvent) => {
+      mouseTrackerX.innerText = mouseEvent.screenX.toString()
+      mouseTrackerY.innerText = mouseEvent.screenY.toString()
+    })
+  ).subscribe()
